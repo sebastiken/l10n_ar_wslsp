2f379574696890b59297f5cdb718b9c9d597073a
Eric Horvat <eric.nahuel.horvat@gmail.com>
2018-03-21 17:51:32 -0300

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+
+#
+#    Copyright (c) 2018 Eynes (Eynes)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+##############################################################################
+
+{
+    "name": "l10n_ar_wslsp",
+    'category': "Others",
+    'version': "8.0.1.0.0",
+    "author": "Eynes",
+    "description": "AFIP livestock sector liquidation webservice (LSLWS - WSLSP) management",
+    "depends": [
+        "base", 
+    ],
+    "data": [
+    ],
+    
+    "installable": True,
+    "application": True,
+}
